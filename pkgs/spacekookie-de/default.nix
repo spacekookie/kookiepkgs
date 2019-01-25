cff834bb6e00732d6ee22566f787b78a3964ce42
Katharina Fey <kookie@spacekookie.de>
2019-01-25 22:35:18 +0100

    Moving the spacekookie.de package data to meta.json

--- pkgs/spacekookie-de/default.nix
+++ pkgs/spacekookie-de/default.nix
@@ -1,27 +1,31 @@
-{ stdenv, fetchFromGitHub, pkgs, ... }:
+{ stdenv, fetchFromGitHub, nix-prefetch-github, pkgs, ... }:
 
-stdenv.mkDerivation rec {
-  version = "2019-01-24";
-  name = "spacekookie.de";
+let
+  json = with builtins; fromJSON (readFile ./meta.json);
+  master = json.rev;
+  masterSha256 = json.sha256;
+in
+  stdenv.mkDerivation rec {
+    name = "spacekookie.de";
 
-  src = fetchFromGitHub {
-    owner = "spacekookie";
-    repo = "website";
-    rev = version;
-    sha256 = "1x7qjwdn7hipx7y1xbb60227fxn99a37knhqqjwcgq22pb4fmbnm";
-  }; 
+    src = fetchFromGitHub {
+      owner = "spacekookie";
+      repo = "website";
+      rev = master;
+      sha256 = masterSha256;
+    }; 
 
-  buildInputs = with pkgs.pythonPackages; [ pelican webassets markdown ];
+    buildInputs = with pkgs.pythonPackages; [ pelican webassets markdown ];
 
-  installPhase = ''
-    pelican content
-    cp -rv output $out
-  '';
+    installPhase = ''
+      pelican content
+      cp -rv output $out
+    '';
 
-  meta = with stdenv.lib; {
-    description = "The `about` and `blog` part of spacekookie.de";
-    homepage = "https://spacekookie.de";
-    license = licenses.mit;
-  };
-}
+    meta = with stdenv.lib; {
+      description = "The `about` and `blog` part of spacekookie.de";
+      homepage = "https://spacekookie.de";
+      license = licenses.mit;
+    };
+  }
 
